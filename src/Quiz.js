3c9f1322df884be06a721ca691ea2a5c08920f8e
Adam Parker <amichaelparker@gmail.com>
2018-08-07 14:43:07 -0500

    add answer checking logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,12 +11,22 @@ class Quiz extends Component {
             quiz_position: 1,
         }
     }
+
+    showNextQuestion() {
+        this.setState({
+            quiz_position: this.state.quiz_position + 1
+        })
+    }
+
     render() {
         const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length ? true : false;
         return(
             (isQuizEnd) ?
             <QuizEnd /> :
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+            <QuizQuestion
+                quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                showNextQuestionHandler={this.showNextQuestion.bind(this)}
+            />
         );
     }
 }
